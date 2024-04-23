593075485b9d8a35d8c7d711eb285b63772bff05
alicechang29 <147457604+alicechang29@users.noreply.github.com>
2024-04-23 16:29:51 -0700

    comments

--- wordfinder.py
+++ wordfinder.py
@@ -8,22 +8,23 @@ class WordFinder:
     def __init__(self, filepath):
         """Take a filepath and read a file that contains 1 word per line,
         create a list of the words, and prints the number of words read"""
-        self.filepath = filepath
-        self.file = open(self.filepath)
+        self.filepath = filepath  # TODO: no need to hold on to this
+        self.file = open(self.filepath)  # TODO: no need to hold on to this
+        # TODO: no need to hold onto the file here
         self.word_list = self.get_lines_in_file(self.file)
         print(f"{len(self.word_list)} words read")
 
     def __repr__(self):
         return f"""
         <WordFinder filepath={self.filepath}
-        word_list={self.word_list}>
+        word_list={self.word_list}> #TODO: change to length of the word list
         """
 
     def get_lines_in_file(self, file):
         """Return a list of the lines in a file"""
         return [line.rstrip() for line in file]
 
-    def random(self):
+    def random(self):  # TODO: import the choice fn for random
         """Generate a random index for a list and return the element at that
          index"""
         rand_index = math.floor(random.random() * len(self.word_list))
@@ -45,6 +46,7 @@ class SpecialWordFinder(WordFinder):
         word_list ={self.word_list}>
         """
 
+    # if this was changed to get_word_list, this subclass will find this fn and not the one in the parent
     def filter_word_list(self, word_list):
         """Take a list of words and return a new list of the words that do
         not start with # or a blank space"""
