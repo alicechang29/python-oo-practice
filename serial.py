ef4bdb333e43c90155fb015ef4b2643d4ad35758
alicechang29 <147457604+alicechang29@users.noreply.github.com>
2024-04-23 14:57:38 -0700

    serial

--- serial.py
+++ serial.py
@@ -1,6 +1,6 @@
 class SerialGenerator:
     """Machine to create unique incrementing serial numbers.
-    
+
     >>> serial = SerialGenerator(start=100)
 
     >>> serial.generate()
@@ -17,3 +17,20 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+
+    def __init__(self, start):
+        """Create serial generator from start number"""
+        self.start = start
+        self.count = self.start
+
+    def __repr__(self):
+        return f"<SerialGenerator start={self.start}, count={self.count}>"
+
+    def generate(self):
+        """Increases count of serial number by 1"""
+        self.count += 1
+        return self.count-1
+
+    def reset(self):
+        """Resets count of serial number to start number"""
+        self.count = self.start
